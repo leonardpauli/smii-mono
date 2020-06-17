357741aff59ffd4cdbd6ac80dc152b81efcd741f
Leonard Pauli <leonardpauli@me.com>
2020-06-17 20:36:17 +0200

    exploring dataset

--- src/snippets/video_yt_fetcher_snippets.js
+++ src/snippets/video_yt_fetcher_snippets.js
@@ -3,7 +3,10 @@ const fs = require('fs')
 const {yt_api} = require('../yt_api.js')
 const {fs_json_file_read} = require('@leonardpauli/utils/src/fs.js')
 const {dlog} = require('@leonardpauli/utils/src/log.js')
-const {identity, obj_entries_map, xs_group} = require('@leonardpauli/utils/src/misc.js')
+const {
+	identity, obj_entries_map, obj_map, xs_group,
+	xs_overview, xs_number_overview,
+} = require('@leonardpauli/utils/src/misc.js')
 const {queries, example_channel_raw} = require('../queries.js')
 
 const {config} = require('../../config.js')
@@ -29,34 +32,20 @@ async function main () {
 	}
 }
 
-const yt_api__i18n_regions = async ()=> {
-	const get_raw = async ()=> {
-		const cachefile = './local/yt_api.i18n_regions.json'
-		const cache = await fs_json_file_read(cachefile)
-		if (cache) return cache
-
-		const d = await yt_api.i18n_regions()
-		await fs.promises.writeFile(cachefile, JSON.stringify(d))
-		return d
-	}
-
-	const raw = await get_raw()
-	const items = raw && raw.data && raw.data.items || []
-	return items.map(v=> ({id: v.id, title: v.snippet && v.snippet.name || null}))
-}
-
 async function nordvpn_clean () {
 
 	dlog({at: 'nordvpn_clean', load: 'nordvpn_rado_v2.json'})
 
 	const raw = require('../../local/nordvpn_rado_v2.json')
 
+	console.dir(xs_overview(raw.slice(2)), {depth: 3})
+	return
 
 	// inspect profile_links
 	if (true) {
 		const extraction = extract_fields_grouped(raw, {
 			field_get: row=> row.profile_link,
-			parse: yt_url_parse,
+			parse: yt_api__channel_url_parse,
 			group_get: parsed=> parsed.type,
 		})
 		extraction.groups = obj_entries_map([...extraction.groups],
@@ -66,37 +55,15 @@ async function nordvpn_clean () {
 
 	// inspect country
 	if (true) {
-		const country_code_get = await country_code_get_get()
+		const country_code_normalise = await country_code_normalise_get()
 		const info = {
-			country: histogram_get(raw, {key_get: row=> country_code_get(row.country)})
+			country: histogram_get(raw, {key_get: row=> country_code_normalise(row.country)})
 		}
 		console.dir(info, {depth: 3})
 	}
 
 }
 
-const country_code_get_get = async ()=> {
-	const regions = await yt_api__i18n_regions()
-	regions.map(a=> console.dir(a))
-	
-	const map = new Map()
-	regions.map(r=> {
-		map.set(r.id.toLowerCase(), r.id)
-		r.title && map.set(r.title.toLowerCase(), r.id)
-	})
-
-	return str=> {
-		const res = map.get((str||'').trim().toLowerCase())
-		return res?res:'failed '+str
-	}
-}
-
-const histogram_get = (xs, {key_get = identity, limit = 0} = {})=> {
-	const groups = xs_group(xs, {key_get})
-	const histogram = Object.fromEntries([...groups]
-		.map(([k, v])=> [k, v.length]).filter(([k, c])=> c>limit))
-	return histogram
-}
 
 const extract_fields_grouped = (xs, {
 	field_get = o=> o.id,
@@ -121,7 +88,41 @@ const extract_fields_grouped = (xs, {
 	}	
 }
 
-const yt_url_parse = str=> {
+
+const country_code_normalise_get = async ()=> {
+	const regions = await yt_api__i18n_regions()
+	regions.map(a=> console.dir(a))
+	
+	const map = new Map()
+	regions.map(r=> {
+		map.set(r.id.toLowerCase(), r.id)
+		r.title && map.set(r.title.toLowerCase(), r.id)
+	})
+
+	return str=> {
+		const res = map.get((str||'').trim().toLowerCase())
+		return res?res:'failed '+str
+	}
+}
+
+
+const yt_api__i18n_regions = async ()=> {
+	const get_raw = async ()=> {
+		const cachefile = './local/yt_api.i18n_regions.json'
+		const cache = await fs_json_file_read(cachefile)
+		if (cache) return cache
+
+		const d = await yt_api.i18n_regions()
+		await fs.promises.writeFile(cachefile, JSON.stringify(d))
+		return d
+	}
+
+	const raw = await get_raw()
+	const items = raw && raw.data && raw.data.items || []
+	return items.map(v=> ({id: v.id, title: v.snippet && v.snippet.name || null}))
+}
+
+const yt_api__channel_url_parse = str=> {
 	const yt_url_parse_regex = /www.youtube.com\/([^\/]*)\/([^\/]+)/
 	const m = str.match(yt_url_parse_regex)
 	if (!m) return null
@@ -129,5 +130,6 @@ const yt_url_parse = str=> {
 	if (!['channel', 'user'].includes(type)) return null
 	return {type, id}
 }
+
 
 module.exports = {main}
