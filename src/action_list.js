49e71c903fee9e96fb970f7a372a813e151914a1
Leonard Pauli <leonardpauli@me.com>
2020-07-01 20:50:59 +0200

    fuzzy search update for Slug

--- src/action_list.js
+++ src/action_list.js
@@ -26,7 +26,7 @@ const action_list = [{
 	query: `
 		with $q as q
 		optional match (c_id:Channel_yt {id: q})
-		optional match (c_slug:Channel_yt {slug: q})
+		optional match (:Slug {id: q})-[:has_channel]->(c_slug:Channel_yt)
 		with q, collect(c_id)+collect(c_slug) as exact_match
 		with q, exact_match, size(exact_match)>0 as has_exact
 		call apoc.do.when(has_exact,
