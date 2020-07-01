2dffc6a6c8179ade16b3a659d73574c1e8d1cd0f
Leonard Pauli <leonardpauli@me.com>
2020-07-01 21:48:20 +0200

    action: add channels by id fix

--- src/action_list.js
+++ src/action_list.js
@@ -152,9 +152,9 @@ const action_list = [{
 	}),
 }, {
 	type: 'neo4j_query',
-	title: 'queue.add.channels.by_id_once',
+	title: 'queue.add.channels.by_id',
 	param_get: ctx=> ({xs: ctx.payload.xs}),
-	query: ctx=> fetcher_queries['queue add channels by id once']({
+	query: ctx=> fetcher_queries['queue add channels once']({
 		xs: '$xs', priority: parseFloat(ctx.payload.priority) || 1}),
 }]
 
